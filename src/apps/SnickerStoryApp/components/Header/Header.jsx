cd4c7f516b54ec7799024b2135a848c1effc6f41
Frellfrell <ella.gubareva.24@gmail.com>
2025-09-22 22:29:01 +0200

    Update Header.jsx

--- src/apps/SnickerStoryApp/components/Header/Header.jsx
+++ src/apps/SnickerStoryApp/components/Header/Header.jsx
@@ -7,9 +7,24 @@ const Header = () => {
     <header className={styles.header}>
       <h1 className={styles.logo}>Сникер-магазин</h1>
       <nav className={styles.nav}>
-        <NavLink to="/">Главная</NavLink>
-        <NavLink to="/cart">Корзина</NavLink>
-        <NavLink to="/contacts">Контакты</NavLink>
+        <NavLink to="/"
+         className={styles.navLink} 
+          style={({ isActive }) => ({
+            color: isActive ? '#FFFFFF' : 'rgba(255,255,255,0.5)'
+          })}> 
+        Главная</NavLink>
+        <NavLink to="/cart"
+         className={styles.navLink} 
+          style={({ isActive }) => ({
+            color: isActive ? '#FFFFFF' : 'rgba(255,255,255,0.5)'
+          })}> 
+        Корзина</NavLink>
+        <NavLink to="/contacts"
+         className={styles.navLink} 
+          style={({ isActive }) => ({
+            color: isActive ? '#FFFFFF' : 'rgba(255,255,255,0.5)'
+          })}> 
+        Контакты</NavLink>
       </nav>
     </header>
   );
