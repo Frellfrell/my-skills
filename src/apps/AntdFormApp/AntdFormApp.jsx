10b55006ffc94967a4591b536ab8d56c29edd086
Frellfrell <ella.gubareva.24@gmail.com>
2025-09-25 11:29:25 +0200

    add impots

--- src/apps/AntdFormApp/AntdFormApp.jsx
+++ src/apps/AntdFormApp/AntdFormApp.jsx
@@ -1,1 +1,6 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Card, Typography } from "antd";
+import { motion, AnimatePresence } from "framer-motion";
 
+
+const { Title, Paragraph } = Typography;
