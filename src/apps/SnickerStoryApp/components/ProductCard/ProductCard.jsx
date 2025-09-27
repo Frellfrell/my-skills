a783f11f61eafdddc52ad8adcc177a9cc3f82204
Frellfrell <ella.gubareva.24@gmail.com>
2025-09-27 20:46:42 +0200

    add cart.json

--- src/apps/SnickerStoryApp/components/ProductCard/ProductCard.jsx
+++ src/apps/SnickerStoryApp/components/ProductCard/ProductCard.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import styles from "./ProductCard.module.css";
 import products from "../../aproducts.json";
 
+import React from "react";
+import styles from "./ProductCard.module.css";
+import products from "../../aproducts.json";
+
 const ProductCard = () => {
   return (
     <div className={styles.productContainer}>
       {products.map((product) => (
         <div key={product.id} className={styles.card}>
-  <img
-    src={require(`../../assets/${product.image}`)}
-    alt={product.name}
-    className={styles.cardImage}
-  />
-  <h3 className={styles.cardTitle}>{product.name}</h3>
-  <div className={styles.cardPriceBox}>
-    <span className={styles.priceLabel}>Цена:</span>
-    <span className={styles.cardPrice}>{product.price}</span>
-    <button className={styles.addButton}>
-      <img
-        src={require(`../../assets/${product.buttonImage}`)}
-        alt="Добавить"
-      />
-    </button>
-  </div>
-</div>
+          <img
+            src={require(`../../assets/${product.image}`)}
+            alt={product.name}
+            className={styles.cardImage}
+          />
+          <h3 className={styles.cardTitle}>{product.name}</h3>
+          <div className={styles.cardPriceBox}>
+            <span className={styles.priceLabel}>Цена:</span>
+            <span className={styles.cardPrice}>{product.price}</span>
+            <button className={styles.addButton}>
+              <img
+                src={require(`../../assets/${product.buttonImage}`)}
+                alt="Добавить"
+              />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
